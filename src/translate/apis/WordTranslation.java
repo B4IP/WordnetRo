fd0cfc67475f212b28180d8d2233d1c12c9b6307
berendeanicolae <berendeanicolae@gmail.com>
2015-05-14 13:44:07 +0300

    start wordtranslation class

---
+++ src/translate/apis/WordTranslation.java
@@ -0,0 +1,31 @@
+package apis;
+
+import java.util.ArrayList;
+
+public class WordTranslation {
+	private String src;
+	private ArrayList<String> dst;
+	
+	public WordTranslation(String src){
+		this.src = src;
+		dst = new ArrayList<>();
+	}
+	
+	public String getSrc() {
+		return src;
+	}
+	
+	private void update(){
+		//update score...
+	}
+	
+	public void addTranslation(String translation){
+		dst.add(translation);
+	}
+	
+	public void addTranslations(ArrayList<String> translations){
+		for (String translation : translations){
+			addTranslation(translation);
+		}
+	}
+}
