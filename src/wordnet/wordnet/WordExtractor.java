9ce99e52b1cd693ba88bf5e3f86d59fbe3276ab2
MihaiCimpan <mihai.cimpan@info.uaic.ro>
2015-05-18 12:50:19 +0300

    Update WordExtractor.java

--- src/wordnet/wordnet/WordExtractor.java
+++ src/wordnet/wordnet/WordExtractor.java
@@ -1,7 +1,5 @@
 // a se vedea metoda iterateWords pentru un exemplu de interactiune
 // intre WordExtractor (ce foloseste API-ul Wordnetului) si baza de date 
-
-
 package wordnet;
 
 import net.sf.extjwnl.JWNLException;
@@ -17,7 +15,6 @@ import java.util.Iterator;
 import database.WordnetDataBase;
 import java.sql.SQLException;
 
-
 /**
  *
  * @author Andrei Pricop
@@ -26,40 +23,138 @@ import java.sql.SQLException;
 public class WordExtractor {
 
     private Dictionary dictionary;
-    
-    public static void main(String[] args) 
-        throws FileNotFoundException, JWNLException, CloneNotSupportedException, SQLException  {
+
+    public static void main(String[] args)
+            throws FileNotFoundException, JWNLException, CloneNotSupportedException, SQLException {
         WordExtractor we = new WordExtractor(Dictionary.getDefaultResourceInstance());
-        we.extractWords("[a](.)*");
+         we.extractWords("[a](.)*");       
+         we.extractHyper("[a](.)*");
+         we.extractMero("[a](.)*");
+        // WordnetDataBase.runDataBaseScript(); 
         WordnetDataBase.closeDataBaseConnection();
-       //we.extractHyper("[a](.)*");
-       //we.extractMero("[a](.)*");
     }
-       
+
     public WordExtractor(Dictionary dictionary) throws SQLException {
         this.dictionary = dictionary;
     }
 
-    public void extractMero(String regex) throws JWNLException {
-        iterateWordsMero(getDictionary().getIndexWordIterator(POS.NOUN), regex);
-        iterateWordsMero(getDictionary().getIndexWordIterator(POS.VERB), regex);
-        iterateWordsMero(getDictionary().getIndexWordIterator(POS.ADJECTIVE), regex);
-        iterateWordsMero(getDictionary().getIndexWordIterator(POS.ADVERB), regex);
+    public void extractWords(String regex) throws JWNLException, SQLException {
+        Iterator<IndexWord> nounIterator = getDictionary().getIndexWordIterator(POS.NOUN);
+        Iterator<IndexWord> verbIterator = getDictionary().getIndexWordIterator(POS.VERB);
+        Iterator<IndexWord> adjectiveIterator = getDictionary().getIndexWordIterator(POS.ADJECTIVE);
+        Iterator<IndexWord> adverbeIterator = getDictionary().getIndexWordIterator(POS.ADVERB);
+
+        iterateWords(findStartPosition(nounIterator, regex), regex);
+        iterateWords(findStartPosition(verbIterator, regex), regex);
+        iterateWords(findStartPosition(adjectiveIterator, regex), regex);
+        iterateWords(findStartPosition(adverbeIterator, regex), regex);
     }
 
-    private void iterateWordsMero(Iterator<IndexWord> iterator, String regex) throws JWNLException {
+    public void extractHyper(String regex) throws JWNLException, SQLException {
+        Iterator<IndexWord> nounIterator = getDictionary().getIndexWordIterator(POS.NOUN);
+        Iterator<IndexWord> verbIterator = getDictionary().getIndexWordIterator(POS.VERB);
+        Iterator<IndexWord> adjectiveIterator = getDictionary().getIndexWordIterator(POS.ADJECTIVE);
+        Iterator<IndexWord> adverbeIterator = getDictionary().getIndexWordIterator(POS.ADVERB);
+
+        iterateWordsHyper(findStartPosition(nounIterator, regex), regex);
+        iterateWordsHyper(findStartPosition(verbIterator, regex), regex);
+        iterateWordsHyper(findStartPosition(adjectiveIterator, regex), regex);
+        iterateWordsHyper(findStartPosition(adverbeIterator, regex), regex);
+    }
+
+    public void extractMero(String regex) throws JWNLException, SQLException {
+        Iterator<IndexWord> nounIterator = getDictionary().getIndexWordIterator(POS.NOUN);
+        Iterator<IndexWord> verbIterator = getDictionary().getIndexWordIterator(POS.VERB);
+        Iterator<IndexWord> adjectiveIterator = getDictionary().getIndexWordIterator(POS.ADJECTIVE);
+        Iterator<IndexWord> adverbeIterator = getDictionary().getIndexWordIterator(POS.ADVERB);
+
+        iterateWordsMero(findStartPosition(nounIterator, regex), regex);
+        iterateWordsMero(findStartPosition(verbIterator, regex), regex);
+        iterateWordsMero(findStartPosition(adjectiveIterator, regex), regex);
+        iterateWordsMero(findStartPosition(adverbeIterator, regex), regex);
+    }
+
+    private void iterateWords(Iterator<IndexWord> iterator, String regex) throws SQLException {
+        int idEnglishWord = WordnetDataBase.getIdCuvantEngleza();
+        int idCuvantRomana = WordnetDataBase.getIdCuvantRomana();
+        IndexWord indexWord;
+        String word;
+        String gloss;
+        String traducere = "Traducere";
         while (iterator.hasNext()) {
-            IndexWord indexWord = iterator.next();
+            indexWord = iterator.next();
+            word = indexWord.getLemma();
+            if (word.matches(regex)) {
+                WordnetDataBase.insertEnglishWords(idEnglishWord, word);
+                System.out.println("---------------------------");
+                System.out.println(idEnglishWord + "  " + word);
+                for (int i = 0; i < indexWord.getSenses().size(); i++) {
+                    gloss = indexWord.getSenses().get(i).getSynset().getGloss();
+                    // traducere = modul3Traducere(word, gloss);
+                    WordnetDataBase.insertRomanianWords(idCuvantRomana, traducere, word, gloss, idEnglishWord);
+                    
+                    System.out.print("     ");
+                    System.out.print("@ " + idCuvantRomana + " @    ");
+                    System.out.print("@ " + traducere + " @    ");
+                    System.out.print("@ " + word + " @    ");                    
+                    System.out.println("@ " + idEnglishWord + " @    ");
+                    System.out.print("     --> glossa: @" + gloss + " @    ");
+                    System.out.println();
+                    idCuvantRomana++;
+                }
+                idEnglishWord++;
+            } else {
+                break;
+            }
+        }
+        WordnetDataBase.setIdCuvantEngleza(idEnglishWord);
+        WordnetDataBase.setIdCuvantRomana(idCuvantRomana);
+    }
+
+    private void iterateWordsHyper(Iterator<IndexWord> iterator, String regex) throws JWNLException, SQLException {
+        String lemma;
+        IndexWord indexWord;
+        while (iterator.hasNext()) {
+            indexWord = iterator.next();           
             if (indexWord.getLemma().matches(regex)) {
+                lemma = indexWord.getLemma();
+                System.out.println("------------------");
+                System.out.println("WordHyper: " + lemma);
+                for (int i = 0; i < indexWord.getSenses().size(); i++) {
+                    PointerTargetNodeList hypernyms = PointerUtils.getDirectHypernyms(indexWord.getSenses().get(i));                 
+                    for (int j = 0; j < hypernyms.size(); j++) {
+                        for (int k = 0; k < hypernyms.get(j).getSynset().getWords().size(); k++) {                          
+                            String hyper = hypernyms.get(j).getSynset().getWords().get(k).getLemma();
+                            if (hyper.matches(regex)) {
+                                // call build relationships
+                                System.out.println("      Hypernym: " + hyper);
+                                WordnetDataBase.insertHypernyms(lemma, hyper);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void iterateWordsMero(Iterator<IndexWord> iterator, String regex) throws JWNLException, SQLException {
+        String lemma;
+        IndexWord indexWord;
+        while (iterator.hasNext()) {
+            indexWord = iterator.next();
+            if (indexWord.getLemma().matches(regex)) {
+                lemma = indexWord.getLemma();
+                System.out.println("------------------");
+                System.out.println("WordMero: " + lemma);
                 for (int i = 0; i < indexWord.getSenses().size(); i++) {
                     PointerTargetNodeList meronyms = PointerUtils.getMeronyms(indexWord.getSenses().get(i));
                     for (int j = 0; j < meronyms.size(); j++) {
                         for (int k = 0; k < meronyms.get(j).getSynset().getWords().size(); k++) {
-                            String lemma = indexWord.getLemma();
                             String mero = meronyms.get(j).getSynset().getWords().get(k).getLemma();
                             if (mero.matches(regex)) {
-                                // call build relationships                              
-                               // System.out.println(lemma + " => " + mero);
+                                System.out.println("      Meronym: " + mero);
+                                WordnetDataBase.insertMeronyms(lemma, mero);                          
+                                // System.out.println(lemma + " => " + mero);
                             }
                         }
                     }
@@ -68,72 +163,18 @@ public class WordExtractor {
         }
     }
 
-    public void extractHyper(String regex) throws JWNLException {
-        iterateWordsHyper(getDictionary().getIndexWordIterator(POS.NOUN), regex);
-        iterateWordsHyper(getDictionary().getIndexWordIterator(POS.VERB), regex);
-        iterateWordsHyper(getDictionary().getIndexWordIterator(POS.ADJECTIVE), regex);
-        iterateWordsHyper(getDictionary().getIndexWordIterator(POS.ADVERB), regex);
-    }
-
-    private void iterateWordsHyper(Iterator<IndexWord> iterator, String regex) throws JWNLException {
+    private Iterator<IndexWord> findStartPosition(Iterator<IndexWord> iterator, String regex)
+            throws SQLException {
         while (iterator.hasNext()) {
-            IndexWord indexWord = iterator.next();
-            if (indexWord.getLemma().matches(regex)) {
-                for (int i = 0; i < indexWord.getSenses().size(); i++) {
-                    PointerTargetNodeList hypernyms = PointerUtils.getDirectHypernyms(indexWord.getSenses().get(i));
-                    for (int j = 0; j < hypernyms.size(); j++) {
-                        for (int k = 0; k < hypernyms.get(j).getSynset().getWords().size(); k++) {
-                            String lemma = indexWord.getLemma();
-                            String hyper = hypernyms.get(j).getSynset().getWords().get(k).getLemma();
-                            if (hyper.matches(regex)) {
-                                // call build relationships
-                                System.out.println(lemma + " => " + hyper);
-                            }
-                        }
-                    }
-                }
+            if (iterator.next().getLemma().matches(regex)) {
+                break;
             }
         }
+        return iterator;
     }
 
-    public void extractWords(String regex) throws JWNLException, SQLException {
-        iterateWords(getDictionary().getIndexWordIterator(POS.NOUN), regex);
-        iterateWords(getDictionary().getIndexWordIterator(POS.VERB), regex);
-        iterateWords(getDictionary().getIndexWordIterator(POS.ADJECTIVE), regex);
-       // iterateWords(getDictionary().getIndexWordIterator(POS.ADVERB), regex);      
-    }
-
-    private void iterateWords(Iterator<IndexWord> iterator, String regex) throws SQLException {
-        
-        int idEng = WordnetDataBase.getIdCuvantEngleza();
-        int idRom = WordnetDataBase.getIdCuvantRomana();
-        String traducere = "Traducere";
-        while (iterator.hasNext()) {
-            IndexWord indexWord = iterator.next();
-            String lemma = indexWord.getLemma();
-            if (indexWord.getLemma().matches(regex)) {
-                 WordnetDataBase.insertEnglishWords(idEng, lemma);  
-                 System.out.println(idEng);
-                for (int i = 0; i < indexWord.getSenses().size(); i++) {                  
-                    String glossa = indexWord.getSenses().get(i).getSynset().getGloss();
-                     // call translate (modulul 3)
-                    // traducere = modulu3Traducere(lemma, glossa);
-                    WordnetDataBase.insertRomanianWords(idRom, traducere, lemma, glossa, idEng);
-                    idRom++;                    		                  
-                    //System.out.println(lemma + " => " + glossa);
-                }
-                idEng++;
-            }
-           // else ...
-           // trebuie sa facem aici o metoda mai eficienta pentru a nu parcurge
-           // de fiecare data toate cuvintele din wordent
-        }      
-        WordnetDataBase.setIdCuvantEngleza(idEng);
-        WordnetDataBase.setIdCuvantRomana(idRom);
-    }
-  
     public Dictionary getDictionary() {
         return dictionary;
     }
-    
+
 }
