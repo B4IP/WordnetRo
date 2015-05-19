dbbc2a0272f36265b6ba83fb2d5492565a865ef4
berendeanicolae <berendeanicolae@gmail.com>
2015-05-19 13:40:05 +0300

    fix utf-8 problem

--- src/translate/http/HttpPost.java
+++ src/translate/http/HttpPost.java
@@ -12,18 +12,21 @@ public class HttpPost {
 	public static String download(String link, String user, String passwd) throws MalformedURLException, IOException{
 		URL url = new URL(link);
 		URLConnection con = url.openConnection();
+		con.setRequestProperty("Accept-Charset", "UTF-8");
 		con.setDoOutput(true);
 
 		String encodedCredentials = Base64.getEncoder().encodeToString((user+":"+passwd).getBytes());
 		con.setRequestProperty("Authorization", "Basic "+encodedCredentials);
 		
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
 		StringBuilder content = new StringBuilder();
 		
 		int read;
 		while ((read=in.read())!=-1){
 			content.append((char)read);
 		}
+		in.close();
+		
 		return content.toString();
 	}
 }
