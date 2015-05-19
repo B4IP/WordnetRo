05185e68cb0a37f6befe0b2fe24ff362720d04b4
FlorinFinaru <florin.finaru@info.uaic.ro>
2015-05-19 18:35:16 +0300

    Translator tests

--- src/translate/tests/TranslatorTest.java
+++ src/translate/tests/TranslatorTest.java
@@ -5,7 +5,19 @@ import translate.translator.Translator;
 public class TranslatorTest {
 
 	public static void main(String[] args) {
+		String word_translated = null;
 		
+		word_translated = Translator.translateFromSentence("game", "The game ended as tie.");
+		System.out.println(word_translated);
+		
+		word_translated = Translator.translateFromSentence("tie", "The game ended as tie.");
+		System.out.println(word_translated);
+		
+		word_translated = Translator.translateFromSentence("little", "Mary had a little lamb.");
+		System.out.println(word_translated);
+		
+		word_translated = Translator.translateFromSentence("santa", "santa and tooth fairy aren't real.");
+		System.out.println(word_translated);
 	}
 
 }
