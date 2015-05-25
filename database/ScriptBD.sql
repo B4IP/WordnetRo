7987faeaa296e96380bca1762043bff44836d14f
MihaiCimpan <mihai.cimpan@info.uaic.ro>
2015-05-25 12:39:21 +0300

    Update on the database tables + added PL/SQL functions for WordnetDatabase class

--- database/ScriptBD.sql
+++ database/ScriptBD.sql
@@ -1,5 +1,3 @@
-SET SERVEROUTPUT ON;
-
 DROP TABLE hiperonime;
 DROP TABLE meronime;
 DROP TABLE cuvinte_romana;
@@ -24,21 +22,16 @@ cuvant_eng VARCHAR2(200) NOT NULL
 
 CREATE TABLE cuvinte_romana (
 ID_cuvant_rom NUMBER PRIMARY KEY,
-cuvant_rom VARCHAR2(200) NOT NULL,
-cuvant_eng VARCHAR2(200) NOT NULL,    
+cuvant_rom VARCHAR2(2000) NOT NULL,
+cuvant_eng VARCHAR2(2000) NOT NULL,    
 glossa VARCHAR2(2000) NOT NULL,
-ID_cuvant_eng_FK NUMBER NOT NULL
+ID_cuvant_eng_FK NUMBER NOT NULL,
+modificat NUMBER DEFAULT 0,
+glossa_tradusa VARCHAR2(2000) DEFAULT 'glossa_lipsa',
+POS VARCHAR2(15) 
 );
 
---INSERT INTO cuvinte_romana VALUES (1, 'cuvant', 'word', 'sensul/glosa', 1);
-
-ALTER TABLE cuvinte_romana
-ADD modificat NUMBER DEFAULT 0;
-
-ALTER TABLE cuvinte_romana
-ADD glossa_tradusa VARCHAR2(2000) DEFAULT 'glossa_lipsa';
-
-
+--INSERT INTO cuvinte_romana VALUES (1, 'cuvant', 'word', 'english gloss', 1, 11, 'glossa tradusa', 'noun');
 
 ALTER TABLE cuvinte_romana 
 ADD CONSTRAINT cuv_rom_ID_cuv_eng_FK FOREIGN KEY ( ID_cuvant_eng_FK ) 
@@ -65,19 +58,70 @@ REFERENCES cuvinte_engleza ( ID_cuvant_eng );
 COMMIT;
 /
 
-DECLARE 
-    numar_cuvinte_engleza INTEGER;
-    numar_cuvinte_romana INTEGER;
-    numar_hiperonime INTEGER;
-    numar_meronime INTEGER;
+CREATE OR REPLACE FUNCTION getContinueTranslatePosition 
+RETURN NUMBER 
+IS
+CURSOR ultimul_tradus IS 
+SELECT modificat, ID_cuvant_rom
+FROM cuvinte_romana;
+ID_romanian_contiune NUMBER :=1 ;
 BEGIN 
-    SELECT COUNT(*) INTO numar_cuvinte_engleza FROM cuvinte_engleza; 
-    SELECT COUNT(*) INTO numar_cuvinte_romana FROM cuvinte_romana; 
-    SELECT COUNT(*) INTO numar_hiperonime FROM hiperonime; 
-    SELECT COUNT(*) INTO numar_meronime FROM meronime; 
-    DBMS_OUTPUT.PUT_LINE('cuvinte_engleza: ' || numar_cuvinte_engleza);
-    DBMS_OUTPUT.PUT_LINE('cuvinte_romana: ' || numar_cuvinte_romana);
-    DBMS_OUTPUT.PUT_LINE('hiperonime: ' || numar_hiperonime);
-    DBMS_OUTPUT.PUT_LINE('meronime: ' || numar_meronime);
+
+    FOR linie IN ultimul_tradus LOOP
+        IF linie.modificat = 1 THEN 
+            ID_romanian_contiune := linie.ID_cuvant_rom;
+        END IF;
+    END LOOP;
+    RETURN ID_romanian_contiune;
 END;
+/
+
+CREATE OR REPLACE FUNCTION getStopPosition 
+RETURN NUMBER 
+IS
+numberOfRows NUMBER :=1 ;
+BEGIN 
+    SELECT COUNT(ID_cuvant_rom) INTO numberOfRows
+    FROM cuvinte_romana;
+    RETURN numberOfRows;
+END;
+/
+
+CREATE OR REPLACE FUNCTION getEnglishWord (p_idCuvantRomana NUMBER) 
+RETURN VARCHAR2
+IS
+englishWord VARCHAR2(2000);
+BEGIN
+   SELECT cuvant_eng INTO englishWord 
+   FROM cuvinte_romana 
+   WHERE ID_cuvant_rom = p_idCuvantRomana;
+   return englishWord;
+END;
+/
+
+
+CREATE OR REPLACE FUNCTION getEnglishGloss (p_idCuvantRomana NUMBER) 
+RETURN VARCHAR2
+IS
+englishGloss VARCHAR2(2000);
+BEGIN
+   SELECT glossa INTO englishGloss 
+   FROM cuvinte_romana 
+   WHERE ID_cuvant_rom = p_idCuvantRomana;
+   return englishGloss;
+END;
+/
+
+CREATE OR REPLACE FUNCTION getEnglishPOS (p_idCuvantRomana NUMBER)
+RETURN VARCHAR2
+IS
+englishPOS VARCHAR2(2000);
+BEGIN 
+    SELECT POS INTO englishPOS 
+    FROM cuvinte_romana 
+    WHERE ID_cuvant_rom = p_idCuvantRomana;
+    RETURN englishPOS;
+END;
+/
+
 
