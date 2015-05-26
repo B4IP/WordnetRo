bcf3239ee638cdc2a3ac1ca9ddfa0c8d83b22979
MihaiCimpan <mihai.cimpan@info.uaic.ro>
2015-05-26 20:29:31 +0300

    Added new function translateFailedTranslations

--- src/wordnet/translator/WordnetTranslator.java
+++ src/wordnet/translator/WordnetTranslator.java
@@ -13,8 +13,12 @@ import translate.translator.Translator;
 public class WordnetTranslator {
 
     public static void translateWords() {
-
+        
+        System.out.println("Called translateWords");
         int idCuvantRomana = WordnetDatabase.getContiunePosition();
+        // probleme cu 6637, 9213, 9214, 9216, 11377, 12453, 12454, 12455, 13248, 14390, 17377
+        
+        
         int stopPosition = WordnetDatabase.getStopPosition();
         System.out.println("Start: " + idCuvantRomana + "\nEnd: " + stopPosition);
 
@@ -38,7 +42,7 @@ public class WordnetTranslator {
             System.out.println(idCuvantRomana + " - " + word);
             
             traducere = translateWord.translate(word, pos, gloss);
-            glossa_tradusa = tranlateGloss.translateSentence(gloss);
+            glossa_tradusa = tranlateGloss.translateSentence(gloss);       
             
             if (glossa_tradusa == null) {
                 glossa_tradusa = gloss;
@@ -50,9 +54,10 @@ public class WordnetTranslator {
                 System.out.println("   %%%%%%%%%%%%%%%%%    ");
                 succes = -1;
             } else {
+               
                 succes = 1;
                 System.out.println("    ---------------------");
-                System.out.println("      " + traducere);
+                System.out.println("      " + "+" + traducere + "+");
                 System.out.println("    ---------------------");
             }
             System.out.println("     --> glossa engleza: @ " + gloss + " @    ");
@@ -60,5 +65,56 @@ public class WordnetTranslator {
             WordnetDatabase.updateRomaninWord(idCuvantRomana, traducere, glossa_tradusa, succes);
             idCuvantRomana++;
         }
+    }
+    
+    public static void translateFailedTranslations() {
+
+        System.out.println("Called translateFailedTranslations");
+        int idCuvantRomana = WordnetDatabase.getContiunePosition();
+        int listOfFailedTraslations[] = WordnetDatabase.getFailedToTranslateIDs();
+
+        String word;
+        String gloss;
+        String glossa_tradusa;
+        POS pos;
+        String traducere;
+        int succes;     
+        Translator translateWord = new Translator();
+        GoogleTranslate tranlateGloss = new GoogleTranslate("en", "ro");
+         
+        for (int i=0;i<listOfFailedTraslations.length;i++)
+        {
+            idCuvantRomana = listOfFailedTraslations[i];
+            word = WordnetDatabase.getEnglishWord(idCuvantRomana);
+            pos = WordnetDatabase.getEnglishPOS(idCuvantRomana);
+            gloss = WordnetDatabase.getEnglishGloss(idCuvantRomana);
+            
+            System.out.println("____________________________________________"
+                    + "____________________________________________");
+            System.out.println(idCuvantRomana + " - " + word);
+            
+            traducere = translateWord.translate(word, pos, gloss);
+            glossa_tradusa = tranlateGloss.translateSentence(gloss);       
+            
+            if (glossa_tradusa == null) {
+                glossa_tradusa = gloss;
+            }
+            if (traducere == null) {
+                traducere = word;
+                System.out.println("   %%%%%%%%%%%%%%%%%    ");
+                System.out.println("     Nu s-a tradus     ");
+                System.out.println("   %%%%%%%%%%%%%%%%%    ");
+                succes = -1;
+            } else {
+               
+                succes = 1;
+                System.out.println("    ---------------------");
+                System.out.println("      " + "+" + traducere + "+");
+                System.out.println("    ---------------------");
+            }
+            System.out.println("     --> glossa engleza: @ " + gloss + " @    ");
+            System.out.println("     --> glossa romana : @ " + glossa_tradusa + " @    ");
+            WordnetDatabase.updateRomaninWord(idCuvantRomana, traducere, glossa_tradusa, succes);    
+        }      
     }
 }
