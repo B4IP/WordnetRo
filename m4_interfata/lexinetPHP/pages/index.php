dd4e58599dda884ad783a865d6ce120c07127be6
MatthewTW <dragos.ivan94@gmail.com>
2015-05-28 22:17:12 +0300

    Changed logo and homepage

--- m4_interfata/lexinetPHP/pages/index.php
+++ m4_interfata/lexinetPHP/pages/index.php
@@ -1,8 +1,6 @@
 <?php require_once('_header.php'); ?>
 <div class="header-intro" >
 		<div class="header-info">
-
-			<h1 class="header-title">Salut, bun venit in wordnet-ul romanesc.</h1>
 			<h2 class="header-subtitle">
 				Introdu un cuvant pentru a afla mai multe informatii despre el.
 			</h2>
